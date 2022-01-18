e33b2c4ecc40e68be5dccca589a29ee701a3bcef
BlittableType <BlittableType@example.com>
2022-01-18 14:37:04 +0300

    feat: Perform hash calculation of each file block in multithreaded manner.

--- FileHashCalculator.cs
+++ FileHashCalculator.cs
@@ -1,69 +1,137 @@
 using System.Threading;
+using System.Security.Cryptography;
 
 namespace BlittableType
 {
-    public class FileHashCalculator
+    public class FileHashCalculator : IDisposable
     {
         private readonly string _filePath;
         private readonly int _blockSize;
         private readonly int _threadCount;
-        private readonly List<Thread> _calcThreads;
+        private List<Thread> _calcThreads;
 
         private long _curPos;
-        private readonly object _curPosSync = new object();
-        private AutoResetEvent _cancelEvent;
+        private int _blockNum;
+        private int _blocks;
+        private readonly object _syncRoot = new object();
+        private CancellationTokenSource _cancelTokenSource;
         
         public FileHashCalculator(string filePath, int blockSize)
         {
             _filePath = filePath;
             _blockSize = blockSize;
-            _threadCount = Environment.ProcessorCount;
+            _curPos = -blockSize;
+            
+            var file = new FileInfo(_filePath);
+            _blocks = (int)Math.Ceiling((decimal)file.Length / _blockSize);
+
+            _threadCount = Math.Min(_blocks, Environment.ProcessorCount);
             _calcThreads = new List<Thread>(_threadCount);
+
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(OnCancelKeyPress);
+        }
+
+        ~FileHashCalculator()
+        {
+            Dispose();
+        }
+
+        private void OnCancelKeyPress(object? sender, ConsoleCancelEventArgs e)
+        {
+            Stop();
+            Console.WriteLine("Operation has been cancelled.");
         }
 
         public void Start()
         {
-            _cancelEvent = new AutoResetEvent(false);
+            _calcThreads = new List<Thread>();
+            _cancelTokenSource = new CancellationTokenSource();
 
             for (int i = 0; i < _threadCount; i++)
             {
-                var thread = new Thread(new ThreadStart(DoCalculation));
+                var thread = new Thread(new ParameterizedThreadStart(DoCalculation));
                 _calcThreads.Add(thread);
-                thread.Start();
+                thread.Start(_cancelTokenSource.Token);
             }
+
+            Console.WriteLine($"Threads: {_threadCount} ({String.Join(", ", _calcThreads.Select(t => t.ManagedThreadId))})");
+            Console.WriteLine($"Blocks: {_blocks}");
+
+            foreach (var thread in _calcThreads)
+                thread.Join();
         }
 
         public void Stop()
         {
-            _cancelEvent.Set();
+            _cancelTokenSource.Cancel();
             
             foreach (var thread in _calcThreads)
                 thread.Join(5000);
-
-            _calcThreads.Clear();
-            _cancelEvent.Close();
         }
 
-        private void DoCalculation()
+        private void DoCalculation(object cancellationTokenObj)
         {
+            var cancellationToken = (CancellationToken)cancellationTokenObj;
+            byte[] block = new byte[_blockSize];
+            long curPos;
+            long blockNum;
+            int bytesRead;
+                        
             using (var file = new FileStream(_filePath, FileMode.Open, FileAccess.Read))
             {
-                byte[] block = new byte[_blockSize];
-
-                while(!_cancelEvent.WaitOne())
-                {
-                    lock(_curPosSync)
+                using (var hashFunc = SHA256.Create())
+                {    
+                    while(!cancellationToken.IsCancellationRequested)
                     {
-                        _curPos += _blockSize;
+                        lock(_syncRoot)
+                        {
+                            curPos = _curPos += _blockSize;
+                            blockNum = ++_blockNum;
+                        }
+
+                        // Read a block of data
+
+                        file.Seek(curPos, SeekOrigin.Begin);                    
+                        bytesRead = file.Read(block, 0, _blockSize);
+
+                        if (bytesRead == 0)
+                            break;
+
+                        // Process last block
+                        
+                        if (bytesRead < _blockSize)
+                        {
+                            var lastBlock = new byte[bytesRead];
+                            Buffer.BlockCopy(block, 0, lastBlock, 0, bytesRead);
+                            CalcAndDisplayHash(hashFunc, lastBlock, blockNum);
+                            break;
+                        }
+
+                        CalcAndDisplayHash(hashFunc, block, blockNum);
                     }
-
-                    file.Seek(_curPos, SeekOrigin.Begin);                    
-                    file.Read(block, 0, _blockSize);
-
-                    // TODO: calculate hash of block
-                    // TODO: display hash value
                 }
             }
+        }
+
+        private void CalcAndDisplayHash(HashAlgorithm hashFunc, byte[] block, long blockNum)
+        {
+            var hash = hashFunc.ComputeHash(block);
+            Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] {blockNum}:{BitConverter.ToString(hash)}");
+        }
+
+        private bool _isDisposed;
+
+        public void Dispose()
+        {
+            if (_isDisposed)
+                return;
+
+            _isDisposed = true;
+
+            Stop();
+            
+            _calcThreads.Clear();
+            _cancelTokenSource.Dispose();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,6 +11,25 @@ namespace BlittableType
                 PrintUsage();
                 return;
             }
+
+            var filePath = args[0];
+
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine($"File {filePath} does not exist.");
+                return;
+            }
+
+            if (!Int32.TryParse(args[1], out int blockSize))
+            {
+                Console.WriteLine($"Block size is in bad format: {args[1]}.");
+                return;
+            }
+
+            using (var fileHashCalculator = new FileHashCalculator(filePath, blockSize))
+            {
+                fileHashCalculator.Start();
+            }
         }
 
         private static void PrintUsage()
